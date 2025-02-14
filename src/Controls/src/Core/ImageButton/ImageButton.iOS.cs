bc2c793552a69036c51eff931911071af4197324
prakashKannanSf3972 <127308739+prakashKannanSf3972@users.noreply.github.com>
2025-02-14 21:21:42 +0530

    Modified-Code

---
+++ src/Controls/src/Core/ImageButton/ImageButton.iOS.cs
@@ -0,0 +1,39 @@
+using System;
+using CoreGraphics;
+using Microsoft.Maui.Graphics;
+using UIKit;
+
+namespace Microsoft.Maui.Controls
+{
+	public partial class ImageButton : ICrossPlatformLayout
+	{
+		/// <summary>
+		/// Measure the desired size of the ImageButton based on the image size taking into account the padding.
+		/// </summary>
+		/// <param name="widthConstraint"></param>
+		/// <param name="heightConstraint"></param>
+		/// <returns>Returns a <see cref="Size"/> representing the width and height of the ImageButton.</returns>
+		/// <remarks>This method is used to override the SizeThatFitsImage() on wrapper view.</remarks>
+		Size ICrossPlatformLayout.CrossPlatformMeasure(double widthConstraint, double heightConstraint)
+		{
+			if (Handler?.PlatformView is not UIButton platformButton)
+				return Size.Zero;
+
+			var result = platformButton.SizeThatFitsImage(
+				new CGSize(widthConstraint, heightConstraint),
+				Padding.IsNaN ? null : Padding);
+
+			return new Size(result.Width, result.Height);
+		}
+
+		/// <summary>
+		/// Returns the size of the ImageButton as a Size based on the specified bounds.
+		/// </summary>
+		/// <param name="bounds"></param>
+		/// <returns>Returns a <see cref="Size"/> representing the width and height of the ImageButton.</returns>
+		Size ICrossPlatformLayout.CrossPlatformArrange(Rect bounds)
+		{
+			return bounds.Size;
+		}
+	}
+}
--- src/Core/src/Platform/iOS/ImageViewExtensions.cs
+++ src/Core/src/Platform/iOS/ImageViewExtensions.cs
@@ -65,32 +65,72 @@ namespace Microsoft.Maui.Platform
 		/// Gets the size that fits on the screen for a <see cref="UIImageView"/> to be consistent cross-platform.
 		/// </summary>
 		/// <remarks>The default iOS implementation of SizeThatFits only returns the image's dimensions and ignores the constraints.</remarks>
-		/// <param name="imageView">The <see cref="UIImageView"/> to be measured.</param>
+		/// <param name="imageSize"></param>
 		/// <param name="constraints">The specified size constraints.</param>
+		/// <param name="contentMode"></param>
+		/// <param name="padding"></param>
 		/// <returns>The size where the image would fit depending on the aspect ratio.</returns>
-		internal static CGSize SizeThatFitsImage(this UIImageView imageView, CGSize constraints)
+		internal static CGSize SizeThatFitsImage(
+			CGSize imageSize,
+			CGSize constraints,
+			UIViewContentMode contentMode,
+			Thickness? padding = null)
 		{
-			// If there's no image, we don't need to take up any space
-			if (imageView.Image is null)
-				return new CGSize(0, 0);
+			if (imageSize.IsEmpty)
+				return CGSize.Empty;
 
-			var heightConstraint = constraints.Height;
-			var widthConstraint = constraints.Width;
-			var imageSize = imageView.Image.Size;
+			var calculatedPadding = padding ?? default(Thickness);
 
-			var widthRatio = Math.Min(imageSize.Width, widthConstraint) / imageSize.Width;
-			var heightRatio = Math.Min(imageSize.Height, heightConstraint) / imageSize.Height;
+			double contentWidth = imageSize.Width + calculatedPadding.HorizontalThickness;
+			double contentHeight = imageSize.Height + calculatedPadding.VerticalThickness;
 
-			// In cases where we the image must fit its given constraints, we must shrink based on the smallest dimension (scale factor)
-			// that can fit it
-			if (imageView.ContentMode == UIViewContentMode.ScaleAspectFit)
+			double constrainedWidth = Math.Min(contentWidth, constraints.Width);
+			double constrainedHeight = Math.Min(contentHeight, constraints.Height);
+
+			double widthRatio = constrainedWidth / imageSize.Width;
+			double heightRatio = constrainedHeight / imageSize.Height;
+
+			// In cases where the image must fit within its given constraints, 
+			// it should be scaled down based on the smallest dimension (scale factor) that allows it to fit.
+			if (contentMode == UIViewContentMode.ScaleAspectFit)
 			{
 				var scaleFactor = Math.Min(widthRatio, heightRatio);
-				return new CGSize(imageSize.Width * scaleFactor, imageSize.Height * scaleFactor);
+				return new CGSize(
+					imageSize.Width * scaleFactor,
+					imageSize.Height * scaleFactor);
 			}
 
 			// Cases where AspectMode is ScaleToFill or Center
-			return constraints;
+			return new CGSize(constrainedWidth, constrainedHeight);
+		}
+
+		/// <summary>
+		/// Extension method for UIImageView
+		/// </summary>
+		internal static CGSize SizeThatFitsImage(this UIImageView imageView, CGSize constraints)
+		{
+			if (imageView.Image is null)
+				return CGSize.Empty;
+
+			return SizeThatFitsImage(
+				imageView.Image.Size,
+				constraints,
+				imageView.ContentMode);
+		}
+
+		/// <summary>
+		/// Extension method for UIButton with image
+		/// </summary>
+		internal static CGSize SizeThatFitsImage(this UIButton button, CGSize constraints, Thickness? padding = null)
+		{
+			if (button.ImageView?.Image is null)
+				return CGSize.Empty;
+
+			return SizeThatFitsImage(
+				button.ImageView.Image.Size,
+				constraints,
+				button.ContentMode,
+				padding);
 		}
 	}
 }
