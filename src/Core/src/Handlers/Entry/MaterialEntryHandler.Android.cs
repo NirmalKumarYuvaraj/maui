04f6bb9e7fa2287bf4ffbd4874187d99856c7a0e
NirmalKumarYuvaraj <97871636+NirmalKumarYuvaraj@users.noreply.github.com>
2025-12-18 15:33:08 +0530

    Implemented material control

---
+++ src/Core/src/Handlers/Entry/MaterialEntryHandler.Android.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.Graphics.Drawables;
+using Android.Text;
+using Android.Views;
+using Android.Views.InputMethods;
+using AndroidX.Core.Content;
+using Google.Android.Material.TextField;
+using Microsoft.Maui.Graphics;
+using static Android.Views.View;
+using static Android.Widget.TextView;
+
+namespace Microsoft.Maui.Handlers
+{
+	internal partial class MaterialEntryHandler : ViewHandler<IEntry, TextInputLayout>
+	{
+		public static PropertyMapper<IEntry, MaterialEntryHandler> Mapper =
+		  new(ElementMapper)
+		  {
+			  [nameof(IEntry.Placeholder)] = MapPlaceholder,
+		  };
+
+		private static void MapPlaceholder(MaterialEntryHandler handler, IEntry entry)
+		{
+			handler.PlatformView.Hint = entry.Placeholder;
+		}
+
+		public static CommandMapper<IEntry, MaterialEntryHandler> CommandMapper =
+		  new(ViewCommandMapper);
+
+		public MaterialEntryHandler() : base(Mapper, CommandMapper)
+		{
+		}
+
+		protected override TextInputLayout CreatePlatformView()
+		{
+			var inflater = LayoutInflater.FromContext(Context)!;
+
+			var view = inflater.Inflate(Resource.Layout.textinputlayout, null);
+			var textInputLayout =
+				view?.FindViewById<TextInputLayout>(Resource.Id.input_outline);
+			//textInputLayout?.SetBoxBackgroundColorResource(Colors.Transparent.ToPlatform());
+			return textInputLayout!;
+		}
+
+	}
+}
---
+++ src/Core/src/Platform/Android/MaterialControls/MauiMaterialEditText.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+using Google.Android.Material.TextField;
+namespace Microsoft.Maui.Platform;
+
+internal class MauiMaterialEditText : TextInputEditText
+{
+	public event EventHandler? SelectionChanged;
+
+	public MauiMaterialEditText(Context context) : base(MauiMaterialContextThemeWrapper.Create(context))
+	{
+	}
+
+	protected override void OnSelectionChanged(int selStart, int selEnd)
+	{
+		base.OnSelectionChanged(selStart, selEnd);
+
+		SelectionChanged?.Invoke(this, EventArgs.Empty);
+	}
+}
+
+internal class MauiMaterialTextInputLayout : TextInputLayout
+{
+	public MauiMaterialTextInputLayout(Context context)
+	 : base(MauiMaterialContextThemeWrapper.Create(context))
+	{
+
+	}
+
+
+
+	// protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+	// {
+	// 	// Get the measure spec mode and size
+	// 	var widthMode = MeasureSpec.GetMode(widthMeasureSpec);
+	// 	var widthSize = MeasureSpec.GetSize(widthMeasureSpec);
+
+	// 	// If we have an AtMost constraint with a specific size, treat it as Exactly
+	// 	// to force the TextInputLayout to expand to the available width
+	// 	// This ensures the Material TextInputLayout fills the available space
+	// 	// instead of wrapping its content
+	// 	if (widthMode == MeasureSpecMode.AtMost && widthSize > 0)
+	// 	{
+	// 		widthMeasureSpec = MeasureSpec.MakeMeasureSpec(widthSize, MeasureSpecMode.Exactly);
+	// 	}
+
+	// 	base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
+	// }
+
+	// public override EditText? EditText => base.EditText as MauiMaterialEditText;
+}
+
